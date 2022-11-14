3dac9012738c1a16ce3c2eb69873981f7410be1d
mich5c18 <mich5c18@edu.ucl.dk>
2022-11-14 13:56:32 +0100

    lavet noget authentication

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using pba_api.Data;
+using pba_api.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace pba_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private IConfiguration _config;
+        private readonly ApplicationDbContext _context;
+
+        public AuthenticationController(IConfiguration config, ApplicationDbContext context)
+        {
+            _config = config;
+            _context = context;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] User user)
+        {
+            var userDto = Authenticate(user);
+
+            if (userDto != null)
+            {
+                var token = Generate(userDto);
+            }
+
+            return NotFound("User not found");
+        }
+
+        private string Generate(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Name),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                    _config["Jwt:Audience"],
+                    claims,
+                    expires: DateTime.Now.AddMinutes(15),
+                    signingCredentials: credentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private User Authenticate(User user)
+        {
+            return _context.Users.FirstOrDefault(o => o.Name.ToLower() == user.Name.ToLower() && o.Password == user.Password);
+        }
+    }
+}
