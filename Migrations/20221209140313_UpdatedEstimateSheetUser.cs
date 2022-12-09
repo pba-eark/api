1dd0c3e9beabb2dedbf53eab9fbef186d5f5f6b2
Rasmus <rasmus.skovhaven@gmail.com>
2022-12-09 15:03:41 +0100

    Added DB migration

---
+++ Migrations/20221209140313_UpdatedEstimateSheetUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pba_api.Migrations
+{
+    public partial class UpdatedEstimateSheetUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "EstimateSheetUsers",
+                type: "varchar(36)",
+                maxLength: 36,
+                nullable: false,
+                defaultValue: "",
+                collation: "utf8mb4_danish_ci")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "EstimateSheetUsers");
+        }
+    }
+}
